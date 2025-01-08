de90a627fdb7f683a16ae4be11d6b393fe2eb785
0xJonaseb11 <sebejaz99@gmail.com>
2025-01-08 17:11:18 +0200

    update #GovernanceDAO

--- Ethereum/contracts/GovernanceDAO.sol
+++ Ethereum/contracts/GovernanceDAO.sol
@@ -9,5 +9,31 @@ pragma solidity ^0.8.20;
 */
 
 contract GovernanceDAO {
+    address[] public members;
+    mapping(address => uint256) public votes;
+
+    constructor(address[] memory initialMembers) {
+        members = initialMembers;
+    }
+
+    // restrict access
+    modifier onlyMember() {
+        bool isMember = false;
+        
+        for (uint i = 0; i < members.length; i++) {
+            if (members[i] == msg.sender) {
+                isMember = true;
+                break;
+            }
+        }
+        require(isMember, "Only DAO members can vote");
+        _;
+    }
+
+    function vote(uint256 proposalId, bool approve) public onlyMember {
+        if (approve) {
+            votes[msg.sender] = proposalId; // real world case, you'd track approval here
+        }
+    }
 
 }
