bcc8fa163abee90d14d603b7e506c12c75c267e7
0xJonaseb11 <sebejaz99@gmail.com>
2025-04-26 20:33:24 +0200

    update: ft interfaces #ICertificateNFT

--- packages/hardhat/contracts/interfaces/ICertificateNFT.sol
+++ packages/hardhat/contracts/interfaces/ICertificateNFT.sol
@@ -3,5 +3,7 @@ pragma solidity ^0.8.20;
 
 interface ICertificateNFT {
     function mintCertificate(address enterprise, string calldata metadataURI) external;
-    
+    function revokeCertificate(uint256 certId) external;
+    function isCertificateValid(uint256 certId) external view returns(bool);
+    function getCertificateId(address enterprise) external view returns(uint256);
 }
