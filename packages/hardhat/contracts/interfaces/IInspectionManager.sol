e87689a9eab8e2668505b610f2df351a78c75979
0xJonaseb11 <sebejaz99@gmail.com>
2025-04-26 18:51:35 +0200

    update: ft interface #IInspectionManager

--- packages/hardhat/contracts/interfaces/IInspectionManager.sol
+++ packages/hardhat/contracts/interfaces/IInspectionManager.sol
@@ -1,3 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+interface IEnspectionManager {
+    function registerInspector(address inspector) external;
+    function removeInspector(address inspector) external;
+    function isInspectorRegistered(address inspeector) external view returns(bool);
+}
