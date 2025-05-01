0d6000a5de281de19393e62d8225e22bbd2f9e67
0xJonaseb11 <sebejaz99@gmail.com>
2025-05-01 08:58:42 +0200

    fix: external contract calling

--- packages/hardhat/contracts/core/AccessModifiers.sol
+++ packages/hardhat/contracts/core/AccessModifiers.sol
@@ -7,7 +7,7 @@ import { RolesManager } from "./RolesManager.sol";
 * @title AccessModifiers
 * @dev provides resusable access control modifiers linnked to RolesManager.
 */
-abstract contract AccessModifiers {
+abstract contract AccessModifiers is RolesManager {
     RolesManager public rolesManager;
 
     constructor(address _rolesManager) {
@@ -15,27 +15,27 @@ abstract contract AccessModifiers {
     } 
 
     modifier onlyCertifier() {
-        require(rolesManager.hasCertifierRole(msg.sender), "Access Denied: Not a registered certifier!");
+        require(hasCertifierRole(msg.sender), "Access Denied: Not a registered certifier!");
         _;
     }
 
     modifier onlyInspector() {
-        require(rolesManager.hasInspectorRole(msg.sender), "Access Denied: Not a registered Inspector");
+        require(hasInspectorRole(msg.sender), "Access Denied: Not a registered Inspector");
         _;
     }
 
     modifier onlyEnterprise() {
-        require(rolesManager.hasEnterpriseRole(msg.sender), "Access Denied: Not a registered Enterprise!!");
+        require(hasEnterpriseRole(msg.sender), "Access Denied: Not a registered Enterprise!!");
         _;
     }
 
     modifier onlyCertificateFactory() {
-        require(rolesManager.hasCertificateFactoryRole(msg.sender), "Access Denied: Not a registered Certificate factory!!");
+        require(hasCertificateFactoryRole(msg.sender), "Access Denied: Not a registered Certificate factory!!");
         _;
     }
 
     modifier onlyAdmin() {
-        require(rolesManager.isAdmin(msg.sender), "Access Denied: Only Admin can accesss this!!");
+        require(isAdmin(msg.sender), "Access Denied: Only Admin can accesss this!!");
         _;
     }
 }
--- packages/nextjs/services/web3/wagmiConfig.tsx
+++ packages/nextjs/services/web3/wagmiConfig.tsx
@@ -1,16 +1,13 @@
+// wagmiConfig.ts
 import { wagmiConnectors } from "./wagmiConnectors";
 import { Chain, createClient, fallback, http } from "viem";
-import { hardhat, mainnet } from "viem/chains";
 import { createConfig } from "wagmi";
-import scaffoldConfig, { DEFAULT_ALCHEMY_API_KEY, ScaffoldConfig } from "~~/scaffold.config";
-import { getAlchemyHttpUrl } from "~~/utils/scaffold-eth";
+import scaffoldConfig from "~~/scaffold.config";
 
-const { targetNetworks } = scaffoldConfig;
+const { targetNetworks, pollingInterval } = scaffoldConfig;
 
-// We always want to have mainnet enabled (ENS resolution, ETH price, etc). But only once.
-export const enabledChains = targetNetworks.find((network: Chain) => network.id === 1)
-  ? targetNetworks
-  : ([...targetNetworks, mainnet] as const);
+// Add logic to include all the networks, ensuring Layer 2s are included.
+export const enabledChains = targetNetworks;
 
 export const wagmiConfig = createConfig({
   chains: enabledChains,
@@ -19,14 +16,13 @@ export const wagmiConfig = createConfig({
   client({ chain }) {
     let rpcFallbacks = [http()];
 
-    const rpcOverrideUrl = (scaffoldConfig.rpcOverrides as ScaffoldConfig["rpcOverrides"])?.[chain.id];
+    const rpcOverrideUrl = scaffoldConfig.rpcOverrides?.[chain.id];
     if (rpcOverrideUrl) {
       rpcFallbacks = [http(rpcOverrideUrl), http()];
     } else {
       const alchemyHttpUrl = getAlchemyHttpUrl(chain.id);
       if (alchemyHttpUrl) {
-        const isUsingDefaultKey = scaffoldConfig.alchemyApiKey === DEFAULT_ALCHEMY_API_KEY;
-        // If using default Scaffold-ETH 2 API key, we prioritize the default RPC
+        const isUsingDefaultKey = scaffoldConfig.alchemyApiKey === "your-api-key";
         rpcFallbacks = isUsingDefaultKey ? [http(), http(alchemyHttpUrl)] : [http(alchemyHttpUrl), http()];
       }
     }
@@ -34,11 +30,7 @@ export const wagmiConfig = createConfig({
     return createClient({
       chain,
       transport: fallback(rpcFallbacks),
-      ...(chain.id !== (hardhat as Chain).id
-        ? {
-            pollingInterval: scaffoldConfig.pollingInterval,
-          }
-        : {}),
+      ...(chain.id !== 31337 ? { pollingInterval } : {}),
     });
   },
 });
