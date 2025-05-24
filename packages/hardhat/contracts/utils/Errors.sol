535eeaf7c4af5b115a06040775312dfcac63e950
0xJonaseb11 <sebejaz99@gmail.com>
2025-05-24 23:51:01 +0200

    update: optimize contracts gas

--- packages/hardhat/contracts/utils/Errors.sol
+++ packages/hardhat/contracts/utils/Errors.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.20;
 
 contract Errors {
     error InvalidAddress();
+
+    // ------- CertificationAuthority
+    error CertificationAuthority__EnterpriseAlreadyCertified();
+    error CertificationAuthority__EnterpriseNotCertifiedYet();
 }
